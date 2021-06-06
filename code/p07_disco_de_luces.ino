a5868f35e0270eb4f37bb20f2f4b12ebb35a92e6
luistaal <luistaal@gmail.com>
2021-06-06 21:35:28 +0200

    Cambios menores
    Añadir control de número y control de colores, cambio en definición de los colores y en la función 'displayColor'

--- code/p07_disco_de_luces.ino
+++ code/p07_disco_de_luces.ino
@@ -5,7 +5,7 @@
   @see https://github.com/FastLED/FastLED
 
   Distribuida bajo licencia GNU General Public License V3
-*/ 
+*/
 #include <FastLED.h>
 
 const uint8_t NUM_LEDS = 30;      // Número de pixeles o LEDs
@@ -13,25 +13,31 @@ const uint8_t DATA_PIN = 2;       // Pin de la tira NeoPixel, D2
 const uint8_t PIN_BUTTON = 3;     // Pin al interruptor, D3
 const uint16_t DELAY_TIME = 1000; // Tiempo de pausa
 const uint8_t BRIGHTNESS = 100;   // Nivel de brillo, entre 0 y 255
+const uint8_t NUM_COLORS = 4;     // Número de colores definidos
 
-uint8_t buttonState = 0; // Estado del interruptor, LOW
-uint8_t count = 0;
+typedef struct Color
+{
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+};
+
+const Color PURPLE = Color{0, 255, 0};
+const Color GREEN = Color{255, 0, 0};
+const Color ROSE = Color{51, 255, 153};
+const Color BLUE = Color{187, 153, 255};
+
+uint8_t buttonState = 0;  // Estado del interruptor, LOW
+uint8_t colorOrder = 0;   // Estado de inicio del orden de color
 
 CRGB leds[NUM_LEDS];
-
-enum Colors {
-  PURPLE,
-  GREEN,
-  ROSE,
-  BLUE
-};
 
 void setup()
 {
   FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
   pinMode(PIN_BUTTON, INPUT);
-  colorLeds(PURPLE);
+  displayColor(PURPLE);
 }
 
 void loop()
@@ -40,53 +46,52 @@ void loop()
 
   if (buttonState == HIGH)
   {
-    count++;
+    colorOrder++;
 
-    if (count > 3)
-      count = 0;
+    if (colorOrder > (NUM_COLORS - 1))
+      colorOrder = 0;
 
     delay(DELAY_TIME);
 
-    colorLeds(count);
+    colorLeds(colorOrder);
   }
 }
 
 /*!
-  @brief   Función para seleccionar el color según el parámetro seleccionado dentro de la lista de 'enum Colors'
+  @brief   Función para seleccionar el color según el valor de la variable 'colorOrder'
   @param   color  Valor de 0 a 255
   @return  Llamada a la función 'displayColor()' con los valores para cada color (r, g, b)
-*/ 
+*/
 void colorLeds(uint8_t color) {
   switch (color) {
-    case PURPLE:
-      displayColor(0, 255, 0);
+    case 0:
+      displayColor(PURPLE);
       break;
-    case GREEN:
-      displayColor(255, 0, 0);
+    case 1:
+      displayColor(GREEN);
       break;
-    case ROSE:
-      displayColor(51, 255, 153);
+    case 2:
+      displayColor(ROSE);
       break;
-    case BLUE:
-      displayColor(187, 153, 255);
+    case 3:
+      displayColor(BLUE);
       break;
   }
 }
 
 /*!
   @brief   Función que muestra los colores en la tira de led según los parámetros obtenidos
-  @param   r  Valor de 0 a 255 para el color del led rojo
-  @param   g  Valor de 0 a 255 para el color del led verde
-  @param   b  Valor de 0 a 255 para el color del led azul
+  @param   color  Valores de 0 a 255 de RGB del color en su variable definida
   @return  Llamada a la función 'setRGB()' con los valores obtenidos
-*/ 
-void displayColor(uint8_t r, uint8_t g, uint8_t b) {
+*/
+void displayColor(Color color)
+{
   FastLED.clear();
 
   for (int dot = 0; dot < NUM_LEDS; dot++)
   {
-    leds[dot].setRGB(r, g, b);
+    leds[dot].setRGB(color.r, color.g, color.b);
   }
 
   FastLED.show();
-}
+};
